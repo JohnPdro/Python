1fb3d40931e6f9fe08744b6d58743199a98fe976
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2026-01-30 15:01:39 -0300

    Classes

---
+++ 3-POO/7-static_method.py
@@ -0,0 +1,26 @@
+"""
+1 - O método estático não utiliza o parâmetro referente a classe.
+2 - O método estático pode acessar mas não pode modificar o estado da classe.
+3 - Usamos o decorator @staticmethod para criar um método estático
+"""
+
+class Course :
+    def __init__(self, name, trail):
+        self.name = name
+        self.trail = trail
+    
+    @staticmethod
+    def courses_trail(trail) :
+        if trail == "Python Fundamentos" :
+            courses = ["Dominando o Python", "Módulos e Pip", "Orientação a Objetos"]
+        elif trail == "Automação com Python" :
+            courses = ["Automação de Tarefas", "Web Scraping", "Assistente Virtual em Python"]
+        else :
+            courses = ["A definir"]
+        
+        return courses
+    
+print(Course.courses_trail("Python Fundamentos"))
+print(Course.courses_trail("Análise de Dados"))
+print(Course.courses_trail("Automação com o Python"))
+
---
+++ 3-POO/8-encapsulamento.py
@@ -0,0 +1,12 @@
+class Employee :
+    def __init__(self, name, salary) :
+        self.name = name
+        self.__salary = salary
+    
+    def show(self) :
+        print(f"Nome {self.name} - Salário {self.__salary}")
+        
+fulano = Employee("Fulano", 4000)
+sicrano = Employee("Sicrano", 5000)
+fulano.show()
+sicrano.show()
